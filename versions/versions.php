8d7f6ff350f00d7973b3b4d2c822281d1d698316
Thor Brink <thor@iseqqavoq.se>
2012-11-28 16:31:04 +0100

    Versions: Added html filter to look for and alter external resources version parameter.
    Signed-off-by: Thor Brink <thor@iseqqavoq.se>

--- versions/versions.php
+++ versions/versions.php
@@ -14,6 +14,7 @@ License:
  *
  * @author	Iseqqavoq
  */
+
 class Versions
 {
 	/**
@@ -21,32 +22,129 @@ class Versions
 	 */
 	public static function init()
 	{
-		add_action( 'wp_print_scripts', array( __CLASS__, 'handle_queued_scripts' ), 999);
+		add_action( 'wp_print_scripts', array( __CLASS__, 'handle_enqueued_scripts' ), 999);
+		add_action( 'wp_print_styles', array( __CLASS__, 'handle_enqueued_styles' ), 999);
+		add_action( 'plugins_loaded', array( __CLASS__, 'start_html_buffer' ) );
+		add_action( 'wp_footer', array( __CLASS__, 'stop_html_buffer' ) );
 	}
 	
 	/**
 	 * Check which scripts in script queue are in need of an altered version number and alter it.
 	 */
-	public static function handle_queued_scripts()
+	public static function handle_enqueued_scripts()
 	{
 		if(is_admin()) return; // If in backoffice, abort.
 		global $wp_scripts;
-		foreach($wp_scripts->registered as $handle => $args)
+		if($wp_scripts)
 		{
-			if( in_array($handle, $wp_scripts->queue ) )
+			foreach($wp_scripts->registered as $handle => $args)
 			{
-				if(!$wp_scripts->registered[$handle]->ver)
+				if( in_array($handle, $wp_scripts->queue ) )
 				{
-					$versionNumber = self::generate_version_number($wp_scripts->registered[$handle]->src);
-					
-					if($versionNumber)
+					if(!$wp_scripts->registered[$handle]->ver)
 					{
-						$wp_scripts->registered[$handle]->ver = $versionNumber;
+						$versionNumber = self::generate_version_number($wp_scripts->registered[$handle]->src);
+						
+						if($versionNumber)
+						{
+							$wp_scripts->registered[$handle]->ver = $versionNumber;
+						}
 					}
+				}
+			}			
+		}
+	}
+	
+	public static function handle_enqueued_styles()
+	{
+		if(is_admin()) return; // If in backoffice, abort.
+		global $wp_styles;
+		if($wp_styles)
+		{
+			foreach($wp_styles->registered as $handle => $args)
+			{
+				if( in_array($handle, $wp_styles->queue ) )
+				{
+					if(!$wp_styles->registered[$handle]->ver)
+					{
+						$versionNumber = self::generate_version_number($wp_styles->registered[$handle]->src);
+						if($versionNumber)
+						{
+							$wp_styles->registered[$handle]->ver = $versionNumber;
+						}
+					}
+				}
+			}			
+		}
+	}
+	
+	/**
+	 * 
+	 *
+	 * @access	public
+	 * @param	string
+	 * @return	string
+	 */
+	public static function start_html_buffer()
+	{
+		ob_start( 'self::filter_output' );
+	}
+	
+	/**
+	 * Filters through the output for external resources that should be altered and alters them.
+	 */
+	public static function filter_output( $buffer )
+	{
+		include_once('simple_html_dom.php');
+		$html = str_get_html($buffer);
+		
+		$links = $html->find('link');
+		
+		$regexp = '/\.css$/';
+		
+		foreach($links as $index => $value)
+		{
+			if(preg_match($regexp, $links[$index]->href)) // If link is a css file...
+			{
+				$versionNumber = self::generate_version_number($links[$index]->href);
+				if($versionNumber)
+				{
+					$links[$index]->href .= '?ver=' . $versionNumber;
 				}
 			}
 		}
+		
+		$scripts = $html->find('script');
+		
+		$regexp = '/\.js$/';
+		
+		foreach($scripts as $index => $value)
+		{
+			if(preg_match($regexp, $links[$index]->href)) // If link is a js file...
+			{
+				$versionNumber = self::generate_version_number($scripts[$index]->href);
+				if($versionNumber)
+				{
+					$scripts[$index]->href .= '?ver=' . $versionNumber;
+				}
+			}
+		}		
+
+		return $html->save();
 	}
+
+	/**
+	 * 
+	 *
+	 * @access	public
+	 * @param	string
+	 * @return	string
+	 */
+	public static function stop_html_buffer()
+	{
+		ob_end_flush();
+	}
+
 		
 	/**
 	 * Generate a version number based on a files filemtime value.
@@ -76,7 +174,8 @@ class Versions
 	{
 		$parsedUrl = parse_url($fileUrl);
 		
-		if($parsedUrl['host'] !== $_SERVER['HTTP_HOST']) return false; // If file is not on server, abort.
+		// If file is not on server, abort.
+		if($parsedUrl && isset($parsedUrl['host']) && $parsedUrl['host'] !== $_SERVER['HTTP_HOST']) return false;
 		
 		$filePath = $_SERVER['DOCUMENT_ROOT'] . $parsedUrl['path'];
 		
