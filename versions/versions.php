6899d68922fb83a5bbbc843434e207e5429b0396
Thor Brink <thor@iseqqavoq.se>
2012-12-11 13:20:36 +0100

    #2: Fixed
    Signed-off-by: Thor Brink <thor@iseqqavoq.se>

--- versions/versions.php
+++ versions/versions.php
@@ -3,7 +3,7 @@
 Plugin Name: Versions
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 0.2
+Version: 1.0
 Author: Iseqqavoq
 Author URI: http://www.iqq.se
 License:
